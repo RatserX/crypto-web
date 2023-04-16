83c97a9148d1b2f554c1618d12d298b79069a15f
RatserX <12604047+RatserX@users.noreply.github.com>
2023-04-16 15:16:00 -0500

    feat: setup formatter and linter

---
+++ .eslintrc.cjs
@@ -0,0 +1,18 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es2021": true
+    },
+    "extends": [
+        "eslint:recommended",
+        "plugin:svelte/recommended"
+    ],
+    "parserOptions": {
+        "ecmaVersion": "latest",
+        "sourceType": "module"
+    },
+    "rules": {
+        "semi": ["error", "always"],
+        "quotes": ["error", "double"]
+    }
+}
